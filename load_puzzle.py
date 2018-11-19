c0c0fe502063fef3c656f3de9757a1b6009c345a
distroJack <jswim26@gmail.com>
2018-11-19 15:03:49 -0500

    Corner extraction 100%

--- load_puzzle.py
+++ load_puzzle.py
@@ -7,6 +7,7 @@ from  scipy.ndimage.filters import maximum_filter
 from scipy.signal import find_peaks
 
 def cart2pol(epts):
+    """ Takes in an Nx2 list of (x,y) points """
     # convert edge points to polar
     x,y = epts[:,0], epts[:,1]
     r = np.sqrt(x**2+y**2)
@@ -16,6 +17,7 @@ def cart2pol(epts):
     return zip(t,r)
 
 def pol2cart(pts):
+    """ Takes in an Nx2 list of (theta,magnitude) points """
     x = np.cos(pts[:,0]) * pts[:,1]
     y = np.sin(pts[:,0]) * pts[:,1]
     
@@ -31,39 +33,28 @@ def getCenter(mask):
 
     return cY, cX
 
-def getOutline(img, showSteps=False):
+def getOutline(img, showSteps, lt=150, ut=240):
+    """ returns a binary mask of the piece"""
 
-    if showSteps:
-        imshow(img)
-
-    timg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    # General purpose kernel used for multiple things
     kernel = np.ones((3,3),np.uint8)
 
-    # Threshold the image
-    lower_t = 150
-    upper_t = 240
-    _, im_th = cv2.threshold(timg, lower_t, upper_t, cv2.THRESH_BINARY_INV);
+    # Threshold the image (base 150, 240)
+    lower_t = lt
+    upper_t = ut
+    _, im_th = cv2.threshold(img, lower_t, upper_t, cv2.THRESH_BINARY_INV);
 
-    if showSteps:
-        plt.title("Threshold of ({}, {})".format(lower_t, upper_t))
-        plt.imshow(im_th)
-        plt.show()
+
+    if im_th[0,0] == ut:
+        return np.array([]),  False
 
     # Detect edges within the image
     lower_e = 12
     upper_e = 75
-    edges = cv2.Canny(timg, upper_e, lower_e)
-
-    if showSteps:
-        plt.title("Edges of ({}, {})".format(lower_t, upper_t))
-        plt.imshow(edges)
-        plt.show()
+    edges = cv2.Canny(img, upper_e, lower_e)
     
     # Combine the thresholded image and the edges together
-    
-    #for i in range(3):
-    im_th[:,:] += edges
+    im_th += edges
 
 
     # Copy the thresholded image.
@@ -75,12 +66,13 @@ def getOutline(img, showSteps=False):
     mask = np.zeros((h+2, w+2), np.uint8)
 
     # Floodfill from point (0, 0)
-    cv2.floodFill(im_floodfill, mask, (0,0), 255);
-     
+    cv2.floodFill(im_floodfill, mask, (0,0), 255);    
+
     # Invert floodfilled image
     im_floodfill_inv = cv2.bitwise_not(im_floodfill)
     cv2.dilate(im_floodfill_inv, kernel, iterations=2)
-     
+    
+
     # Combine the two images to get the foreground.
     imout = im_th | im_floodfill_inv
 
@@ -93,16 +85,35 @@ def getOutline(img, showSteps=False):
     gray_image = imout.copy()#cv2.cvtColor(imout, cv2.COLOR_BGR2GRAY)
     gray_image[gray_image < 255] = 0
 
-    #imshow(gray_image)
+    tltest = gray_image[0,0] == 255
+    trtest = gray_image[0,-1] == 255
+    bltest = gray_image[-1,0] == 255    
+    brtest = gray_image[-1,-1] == 255
+
+    if tltest or trtest or bltest or brtest:
+        return np.array([]),  False
+
     if showSteps:
-        plt.title("Piece Bitmask")
-        plt.imshow(gray_image)
+        plt.title("Threshold of ({}, {})".format(lower_t, upper_t))
+        plt.imshow(im_th)
         plt.show()
 
+    if showSteps:
+        plt.title("Edges of ({}, {})".format(lower_t, upper_t))
+        plt.imshow(edges)
+        plt.show()
+    
+    #gray_image = cv2.erode(gray_image, kernel, iterations = 6)
+    return gray_image, True
 
-    center = getCenter(gray_image)
+def getCorners(img, tval, bfactor=1, showSteps=True):
 
-    edges = cv2.Canny(gray_image,75,12)
+    # General purpose kernel used for multiple things
+    kernel = np.ones((3,3),np.uint8)
+
+    center = getCenter(img)
+
+    edges = cv2.Canny(img,75,12)
 
     if showSteps:
         plt.imshow(edges)
@@ -133,6 +144,7 @@ def getOutline(img, showSteps=False):
     epts = np.array(epts) 
 
     if showSteps:
+        print(tval)
         plt.scatter(epts[:,0], epts[:,1])
         plt.title("Polar Edge points")
         plt.show()  
@@ -143,7 +155,7 @@ def getOutline(img, showSteps=False):
     mx = max(epts[:,1])    
     average = sum(epts[:,1]) / epts.shape[0]
 
-    average = average + mx/16
+    average = (average * bfactor) + mx/tval
 
     # Get all points above average
     outer = epts[epts[:,1] > average]
@@ -160,7 +172,6 @@ def getOutline(img, showSteps=False):
 
     mx = 0
     t = []
-
     for i in range(len(outer) - 1):
         l = outer[i]
         r = outer[i + 1]
@@ -171,16 +182,20 @@ def getOutline(img, showSteps=False):
 
     t = np.array(t)
 
+    if t.size == 0:
+        print("Error : no points of interest found in outer")
+        return False, []
+    
     if showSteps:
 
-        r = t[:,:,0]
-        m = t[:,:,1]
-
+        rad = t[:,:,0]
+        mag = t[:,:,1]
 
         plt.title("Found boundaries")
         plt.scatter(outer[:,0], outer[:,1])
-        plt.scatter(r, m, c='#FF0000')
+        plt.scatter(rad, mag, c='#FF0000')
         plt.show()
+
 
     maxs = []
 
@@ -189,15 +204,21 @@ def getOutline(img, showSteps=False):
         r = t[i + 1][0]
 
         pts = outer[(outer[:,0] > l[0]) & (outer[:,0] < r[0])]
+        #if pts.size == 0:
+        if pts.size == 0:
+            continue
         m = max(pts, key=getT)
-
         maxs.append(m)
 
     # get one of the corners
     pts = outer[outer[:,0] < t[0][0][0]]
+    if pts.size == 0:
+        return [], False
     omx = max(pts, key=getT)
     
     pts = outer[outer[:,0] > t[-1][1][0]]
+    if pts.size == 0:
+        return [], False
     tmx = max(pts, key=getT)
 
     maxs.append(omx)
@@ -220,6 +241,13 @@ def getOutline(img, showSteps=False):
     #if showSteps:
     opts = cpts + center
     ocrn = corners + center
+
+    if showSteps:
+        plt.imshow(img)
+        plt.title("Points on piece")
+        plt.scatter(ocrn[:,1], ocrn[:,0], c='#FF0000')
+        plt.show()
+
     tcorn = np.array([])
     for cand in ocrn:
         test = [cand]
@@ -255,48 +283,100 @@ def getOutline(img, showSteps=False):
                 test.append(mc)
 
 
-
-        
         test = np.array(test)
 
         moreCorners = len(test) > len(tcorn)
         fourCorners = len(test) == 4
-        centerTest = (sum(test[:,0] > center[0]))
+        verticalTest = (sum(test[:,0] > center[0]) == 2)
+        horizontalTest = (sum(test[:,1] > center[1]) == 2)
 
-        if fourCorners:
+        if fourCorners and verticalTest and horizontalTest:
             tcorn = test.copy()
 
-
     if tcorn.size == 0:
-        return False
+        return tcorn, False
+    return tcorn, True
 
+
+def extractPiece(img, lval, showSteps=False):
+
+    
+    # seperate into parts of failure
+    # - if bitmask isnt right we need to fix it
+    # - if edge threshold is too high we need to fix it as well
     if showSteps:
-        plt.imshow(img)
-        plt.title("Points on piece")
-        plt.scatter(ocrn[:,1], ocrn[:,0], c='#FF0000')
+        imshow(img)
+
+    timg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    lower = lval
+    bitmapSuccess = False
+    gray_image = np.array([])
+    while not bitmapSuccess and lower >= 0:
+        #print(lower)
+        gray_image, bitmapSuccess = getOutline(timg, showSteps, lower)
+        lower -= 2
+
+    if not bitmapSuccess:
+        return False, lower
+
+    
+
+    #imshow(gray_image)
+    if showSteps:
+        plt.title("Piece Bitmask")
+        plt.imshow(gray_image)
         plt.show()
+
+    ival = 8
+    cornerSuccess = False
+    while not cornerSuccess:
+        tcorn, cornerSuccess = getCorners(gray_image, ival, 1, showSteps)
+        #print("Testing tval: {} output {}".format(ival, success))
+        ival *= 1.5
+
+        if ival > 100:
+            break
+
+    bfactor = .9
+    while not cornerSuccess and bfactor >= .5:
+        tcorn, cornerSuccess = getCorners(gray_image, ival, bfactor, showSteps)
+        bfactor -= .05
+
+    if not cornerSuccess:
+        return False, lower
 
     plt.imshow(img)
     plt.title("Points on piece")
     plt.scatter(tcorn[:,1], tcorn[:,0], c='#FF0000')
     plt.show()
 
-    return True
+    return True, lower
 
 
 
 if __name__ == "__main__":
 
-    for i in range(8):
-        for j in range(12):
+    for i in range(4,8):
+        for j in range(6,12):
             print(i,j)
             f = "images/moanaIndividual/{}_{}.jpg".format(i,j)
             im_in = cv2.imread(f)
-            test = getOutline(im_in, False)
-            if not test:
+
+            success = False
+            lval = 200
+            while lval >= 100 and not success:
+                success, lval = extractPiece(im_in, lval, False)
+
+            if not success:
                 print("error test {} failed".format(i))
     """
-    im_in = cv2.imread("images/moanaIndividual/6_6.jpg")
-    getOutline(im_in, False)
+    im_in = cv2.imread("images/moanaIndividual/4_9.jpg")
+    success = False
+    lval = 200
+    while lval >= 100 and not success:
+        success, lval = extractPiece(im_in, lval, False)
+        print(lval)
+    print(success)
     """
+
 
